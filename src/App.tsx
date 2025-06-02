cd2eb5d425d4ce6bdbb0e484f5a3305b5dc41f16
shikashika <yaxianghu@gmail.com>
2025-06-02 21:25:21 +0800

    final check

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import './App.css';
 
 function App() {
   const images = [
-    { src: '/give.jpg', description: 'Description 1' },
-    { src: '/listen.jpg', description: 'Description 2' },
-    { src: '/pray.jpg', description: 'Description 3' },
-    { src: '/shock.jpg', description: 'Description 4' },
-    { src: '/throwball.jpg', description: 'Description 5' },
+    { src: '/give.jpg', description: '朋友間最好的贈禮' },
+    { src: '/listen.jpg', description: '我傾聽他的味道' },
+    { src: '/pray.jpg', description: '神啊 他是我月底的救星星' },
+    { src: '/shock.jpg', description: 'Oh my godo, is 蠟燭泡麵' },
+    { src: '/throwball.jpg', description: '百投百中!' },
   ];
 
   return (
     <div className="container">
       <div className="header-overlay">
-       <img src="/background.jpg /" className="header-image" />
+       <img src="/container.jpg" className="header-image" />
        <video
           src="/groupvid.mp4"
           className="header-video"
@@ -23,7 +23,15 @@ function App() {
         />
        
    
-       <h1 className="header-title">My Portfolio</h1>
+       <a 
+        href="https://www.youtube.com/watch?v=k_pjkGfJ_R0&t=1s" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="header-title-block"
+       > 
+        <div className="main-title">實現心願？蠟燭泡麵!</div>
+        <div className="sub-title">（詳情請點擊標題）</div>
+      </a>
     </div>
 
     <div className="image-row">
